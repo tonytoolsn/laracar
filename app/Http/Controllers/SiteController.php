31d867a7b0b74d4493d8029a877cb8cf948ab33e
gh_tony <web109btony712@gmail.com>
2021-04-16 03:38:58 +0800

    newebpay-test

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Models\Category;
 use App\Models\Product;
 use App\Models\Order;
@@ -11,8 +10,9 @@ use App\Models\Calendar;
 use Carbon\Carbon;
 use Auth;
 use App\Http\Requests\OrderRequest;
-
 use App\Models\Slide;
+use Illuminate\Support\Facades\Log;
+use NewebPay;
 
 class SiteController extends Controller
 {
@@ -36,8 +36,39 @@ class SiteController extends Controller
         }
 
         $data = $request->except('_token');
-        $order = Order::create($data);
+        $request->session()->flush();
+        //將表單資料用session()，先存起來
+        $request->session()->push('data',$data);
 
+        $newebpay = new NewebPay();  //使用此物件要記得use
+        $deposit=$request->deposit;
+        $pattern=$request->pattern;
+        $email=$request->email;
+        return $newebpay->payment(
+        Carbon::now()->timestamp,    // 訂單編號
+        $deposit,                    // 交易金額
+        $pattern,                    // 商品名稱
+        $email                       // 付款人信箱
+        )->submit();
+    }
+
+    public function successRedirect(Request $request){
+        $data = $request->session()->get('data');
+            foreach ($data as $value) {
+            $data = $value;
+            }
+        Order::create($data);
+        $request->session()->flush();
         return redirect()->route('root')->with('msg', '已成功下訂單，請至會員專區查看訂單狀態');
+    }
+
+    public function orderSuccess(Request $request){
+        Log::info('app.requests',['request' => $request->all()]); //藍星金流回傳的資料
+        return redirect()->route('root');
+    }
+
+      public function back(Request $request){
+        $request->session()->flush();
+        return redirect()->route('root')->with('msg', '訂單取消!!');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,13 @@ Route::group(['namespace' => '\App\Http\Controllers'], function () {
     Route::get('/edit', 'HomeController@edit')->name('edit');
     Route::patch('/update', 'HomeController@update')->name('update');
     Route::post('/order/submit', 'SiteController@orderSubmit')->name('orderSubmit');
+    //當使用者付款成功時，導到的畫面，是用post，Controller重導get的success方法
+    Route::post('purchases/successRedirect' ,'SiteController@@successRedirect');
+    Route::get('purchases/success','SiteController@@success');
+    //當使用者付款成功，藍星將資料導到的位址，是用post
+    Route::post('purchases/success','SiteController@@orderSuccess');
+    //取消付款導到的頁面，是用get
+    Route::post('purchases/back','SiteController@@back');
 });
 
 Route::group(['prefix' => 'admin'], function () {
